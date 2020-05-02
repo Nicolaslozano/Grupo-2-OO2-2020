d7d2acdbc708a182405797e25f7a10f62284174b
Nicolaslozano <44273672+Nicolaslozano@users.noreply.github.com>
2020-05-02 17:31:12 -0300

    Metodo calcular distancia Testeado

--- src/test/test.java
+++ src/test/test.java
@@ -27,27 +27,37 @@ public class test {
 		CarritoABM carritoABM = CarritoABM.getInstance();
 		PedidoABM pedidoABM = PedidoABM.getInstance();
 		EmpleadoABM empleadoABM = EmpleadoABM.getInstance();
+		SolicitudStockABM SolicitudStock=SolicitudStockABM.getInstance();
 
 		//carritoABM.agregar(LocalDate.now());
 
-		//localABM.agregar("direccion", 555, 555, 555, null);
+		//localABM.agregar("direccion", 1555,1555,1555, null);
 
 		//empleadoABM.agregar("EMPLE", "ADO", LocalDate.now(), 45678, null, false, localABM.traerLocal(1));
 
-		Producto p = productoABM.traerProducto(2);
-		Pedido pp = pedidoABM.traerPedido(1);
+		//Producto p = productoABM.traerProducto(2);
+		//Pedido pp = pedidoABM.traerPedido(1);
 
 		//System.out.println(p.getIdProducto());
 		//pedidoABM.agregar(p, 300, localABM.traerLocal(1), clienteABM.traerCliente(123),
 		 //carritoABM.traerCarrito(1), empleadoABM.traerEmpleado(45678), null, false);
 
-		 System.out.println(pp.getVendedorOriginal().getApellido());
+		// System.out.println(pp.getVendedorOriginal().getApellido());
 		//Local local =localABM.traerLocal(1);
 
 		//System.out.println(local.getTelefono());
 
 		//stockABM.agregar(12, local);
-
+		// localABM.eliminar(1);
+		try {
+			System.out.println(SolicitudStock.calcularDistancia(localABM.traerLocal(2), localABM.traerLocal(3)));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+		 
 
 	}
+	
 }
