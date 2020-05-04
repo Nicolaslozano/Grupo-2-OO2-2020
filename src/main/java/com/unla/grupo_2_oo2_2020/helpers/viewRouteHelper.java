a121a4a4b538396f4727cc7874575ac51e789848
arbp97 <arbp97@outlook.com>
2020-05-04 16:52:40 -0300

    added viewRouteHelper

---
+++ src/main/java/com/unla/grupo_2_oo2_2020/helpers/viewRouteHelper.java
@@ -0,0 +1,20 @@
+package com.unla.grupo_2_oo2_2020.helpers;
+
+public class viewRouteHelper {
+
+    /**** Views ****/
+	//HOME
+    public final static String INDEX = "home/index";
+
+    //CLIENTE
+    public final static String CLIENTE_INDEX = "cliente/index";
+    public final static String CLIENTE_NEW = "cliente/new";
+    public final static String CLIENTE_UPDATE = "cliente/update";
+    public final static String CLIENTE_REMOVE = "cliente/remove";
+
+    //EMPLEADO ...
+
+	/**** Redirects ****/
+	public final static String ROUTE = "/index";
+
+}
