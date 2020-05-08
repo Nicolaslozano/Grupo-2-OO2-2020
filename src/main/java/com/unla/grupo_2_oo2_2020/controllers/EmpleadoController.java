b1291feadb864911994929a81be5f02e76131666
Nicolaslozano <44273672+Nicolaslozano@users.noreply.github.com>
2020-05-08 05:36:48 -0300

    Controlador de Empleado terminado

---
+++ src/main/java/com/unla/grupo_2_oo2_2020/controllers/EmpleadoController.java
@@ -0,0 +1,47 @@
+package com.unla.grupo_2_oo2_2020.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.unla.grupo_2_oo2_2020.helpers.ViewRouteHelper;
+import com.unla.grupo_2_oo2_2020.models.ClienteModel;
+import com.unla.grupo_2_oo2_2020.models.EmpleadoModel;
+import com.unla.grupo_2_oo2_2020.services.IClienteService;
+import com.unla.grupo_2_oo2_2020.services.IEmpleadoService;
+import com.unla.grupo_2_oo2_2020.services.implementation.EmpleadoService;
+
+@Controller
+@RequestMapping("/empleado")
+public class EmpleadoController {
+
+    @Autowired
+	@Qualifier("empleadoService")
+	private IEmpleadoService empleadoService;
+
+    @GetMapping("")
+	public ModelAndView index() {
+		ModelAndView mAV = new ModelAndView(ViewRouteHelper.EMPLEADO_INDEX);
+
+		return mAV;
+	}
+
+    @GetMapping("/new")
+	public ModelAndView create() {
+		ModelAndView mAV = new ModelAndView(ViewRouteHelper.EMPLEADO_NEW);
+		mAV.addObject("cliente", new EmpleadoModel());
+		return mAV;
+	}
+
+	@PostMapping("/create")
+	public RedirectView create(@ModelAttribute("empleado") EmpleadoModel empleadoModel) {
+		empleadoService.insertOrUpdate(empleadoModel);
+		return new RedirectView(ViewRouteHelper.EMPLEADO_ROOT);
+    }
+}
