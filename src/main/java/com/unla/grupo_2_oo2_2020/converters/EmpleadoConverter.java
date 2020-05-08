31893bb6fd95fc7ad3c9bb7f436c212a2610f7d9
Nicolaslozano <44273672+Nicolaslozano@users.noreply.github.com>
2020-05-08 05:42:05 -0300

    Conversor de Empleados(A REVISAR)

---
+++ src/main/java/com/unla/grupo_2_oo2_2020/converters/EmpleadoConverter.java
@@ -0,0 +1,22 @@
+package com.unla.grupo_2_oo2_2020.converters;
+
+import org.springframework.stereotype.Component;
+
+import com.unla.grupo_2_oo2_2020.entities.Empleado;
+import com.unla.grupo_2_oo2_2020.models.EmpleadoModel;
+
+@Component("empleadoConverter")
+public class EmpleadoConverter {
+
+	public EmpleadoModel entityToModel(Empleado empleado) {
+		return new EmpleadoModel(empleado.getIdPersona(), empleado.getNombre(), empleado.getApellido(),
+				empleado.getFechaNacimiento(), empleado.getDni(), empleado.getFranjaHoraria(),
+				empleado.isTipoEmpleado());
+	}
+
+	public Empleado modelToEntity(EmpleadoModel empleadoModel) {
+		return new Empleado(empleadoModel.getIdPersona(), empleadoModel.getNombre(), empleadoModel.getApellido(),
+				empleadoModel.getDni(), empleadoModel.getFechaNacimiento(), empleadoModel.getFranjaHoraria(),
+				empleadoModel.isTipoEmpleado(), empleadoModel);
+	}
+}
