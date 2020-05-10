2bb443220fd70440e039c8beeb09b4061fe95ea0
Nicolaslozano <44273672+Nicolaslozano@users.noreply.github.com>
2020-05-10 17:54:18 -0300

    Añadido Cliente.remove con fallos a revisar

--- src/main/java/com/unla/grupo_2_oo2_2020/controllers/ClienteController.java
+++ src/main/java/com/unla/grupo_2_oo2_2020/controllers/ClienteController.java
@@ -5,11 +5,12 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
-
+import com.unla.grupo_2_oo2_2020.converters.ClienteConverter;
 import com.unla.grupo_2_oo2_2020.helpers.ViewRouteHelper;
 import com.unla.grupo_2_oo2_2020.models.ClienteModel;
 import com.unla.grupo_2_oo2_2020.services.IClienteService;
@@ -23,10 +24,14 @@ public class ClienteController {
 	@Qualifier("clienteService")
 	private IClienteService clienteService;
 
+	@Autowired
+	@Qualifier("clienteConverter")
+	private ClienteConverter clienteConverter;
+
     @GetMapping("")
 	public ModelAndView index() {
 		ModelAndView mAV = new ModelAndView(ViewRouteHelper.CLIENTE_INDEX);
-
+		mAV.addObject("clientes", clienteService.getAll());
 		return mAV;
 	}
 
@@ -41,6 +46,26 @@ public class ClienteController {
 	public RedirectView create(@ModelAttribute("cliente") ClienteModel clienteModel) {
 		clienteService.insertOrUpdate(clienteModel);
 		return new RedirectView(ViewRouteHelper.CLIENTE_ROOT);
-    }
-    
+	}
+
+	@GetMapping("/{idPersona}")
+	public ModelAndView get(@PathVariable("idPersona") long id) {
+		ModelAndView mAV = new ModelAndView(ViewRouteHelper.CLIENTE_UPDATE);
+		mAV.addObject("cliente", clienteService.findById(id));
+		return mAV;
+	}
+
+	@PostMapping("/update")
+	public RedirectView update(@ModelAttribute("cliente") ClienteModel clienteModel) {
+		clienteService.insertOrUpdate(clienteModel);
+		return new RedirectView(ViewRouteHelper.CLIENTE_ROOT);
+	}
+	
+	@PostMapping("/remove/{id}")
+	public RedirectView delete(@PathVariable("idPersona") long idPersona) {
+		clienteService.removeById(idPersona);
+		return new RedirectView(ViewRouteHelper.CLIENTE_REMOVE);
+	}
+	
+	
 }
