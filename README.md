152606a2f6b56b410ff74f8720c835dba586a5b1
arbp97 <arbp97@outlook.com>
2020-05-19 15:12:49 -0300

    disabled default login, updated readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Oo2TP GRUPO 2
+
+Spring Boot 2.2.6, Java 8
+
+Dependencias adicionales (Maven)
+
+- Lombok
+  - Eclipse/STS/Intellij Idea: <https://www.baeldung.com/lombok-ide>
+  - VSCode extension: <https://marketplace.visualstudio.com/items?itemName=GabrielBB.vscode-lombok>
--- src/main/java/com/unla/grupo_2_oo2_2020/config/SecurityConfiguration.java
+++ src/main/java/com/unla/grupo_2_oo2_2020/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.unla.grupo_2_oo2_2020.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,5 +18,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
      public void configure(WebSecurity web) throws Exception {
          web.ignoring().antMatchers("/resources/**");
          // Spring Security should completely ignore URLs starting with /resources/
+     }
+
+     @Override
+     public void configure(HttpSecurity security) throws Exception {
+         security.httpBasic().disable();
      }
 }
