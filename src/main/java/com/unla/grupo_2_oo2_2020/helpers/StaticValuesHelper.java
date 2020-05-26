1a78cead12b1e1a435080933fbdeed2478129a00
Nicolaslozano <44273672+Nicolaslozano@users.noreply.github.com>
2020-05-25 21:26:57 -0300

    Add files via upload

--- src/main/java/com/unla/grupo_2_oo2_2020/helpers/StaticValuesHelper.java
+++ src/main/java/com/unla/grupo_2_oo2_2020/helpers/StaticValuesHelper.java
@@ -7,7 +7,8 @@ public class StaticValuesHelper {
     public final static String SURNAME_REQUIRED = "surname_required";
     public final static String DNI_REQUIRED = "dni_required";
     public final static String BIRTHDATE_REQUIRED = "birthdate_required";
-
+    public final static String LOCAL_REQUIRED = "local_required";
+    public final static String FRANJAHORARIA_REQUIRED = "franjahoraria_required";
     public final static String EMAIL_ALREADY_EXISTS = "email_already_exists";
     public final static String PERSON_ALREADY_EXISTS = "person_already_exists";
 
