795864db78518421aa316e191150af2981e497f4
Nicolaslozano <44273672+Nicolaslozano@users.noreply.github.com>
2020-05-25 21:27:32 -0300

    Add files via upload

--- src/main/java/com/unla/grupo_2_oo2_2020/models/EmpleadoModel.java
+++ src/main/java/com/unla/grupo_2_oo2_2020/models/EmpleadoModel.java
@@ -2,13 +2,20 @@ package com.unla.grupo_2_oo2_2020.models;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import com.unla.grupo_2_oo2_2020.helpers.StaticValuesHelper;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 @Data @EqualsAndHashCode(callSuper = false) @NoArgsConstructor
 public class EmpleadoModel extends PersonaModel {
+	@NotNull(message = StaticValuesHelper.FRANJAHORARIA_REQUIRED)
 	private String franjaHoraria;
 	private boolean tipoEmpleado; // true = Vendedor, false = Gerente.
+	@Min(value=1, message = StaticValuesHelper.LOCAL_REQUIRED)
 	private long idLocal;
 
 	public EmpleadoModel(long idPersona,String nombre, String apellido, LocalDate fechaNacimiento, int dni,
