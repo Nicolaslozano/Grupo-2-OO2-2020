ea3a1383ed5157531f27abb5a9a077106f45f9f4
Nico <nicolas_lozano7@hotmail.com>
2020-05-28 22:39:44 -0300

    WIP STOCK OTRO LOCAL

--- src/main/java/com/unla/grupo_2_oo2_2020/services/implementation/PedidoService.java
+++ src/main/java/com/unla/grupo_2_oo2_2020/services/implementation/PedidoService.java
@@ -1,5 +1,6 @@
 package com.unla.grupo_2_oo2_2020.services.implementation;
 
+import com.unla.grupo_2_oo2_2020.entities.Local;
 import com.unla.grupo_2_oo2_2020.models.PedidoModel;
 import com.unla.grupo_2_oo2_2020.services.ILocalService;
 import com.unla.grupo_2_oo2_2020.services.ILoteService;
@@ -30,23 +31,33 @@ public class PedidoService implements IPedidoService {
     @Qualifier("loteService")
     private ILoteService loteService;
 
-    @Override
-    public boolean validatePedido(PedidoModel pedidoModel) {
-        // TODO
+	@Override
+	public boolean validatePedido(PedidoModel pedidoModel) {
 
-        // stock service validate stock (pedidomodel)
+		boolean DemandFullFill = stockService.comprobarStock(pedidoModel);
+		long i = 0;
 
-        // if true, then lote.cantidad - pedido.cantidad and save it
-        // if local has a bit, then send solicitud of only needed stock to closest local
-        // else if false, then send solicitud to closest local that has enough
-        // if no one has, then IMSORRYBUDDY
-        return stockService.comprobarStock(pedidoModel);
-    }
+		while (DemandFullFill == false && i < localService.getAll().size()) {
+		 Local local = localService.findById(i); 
+			if (local != null) {
+				if (cederStock() = true) {
+					stockService.comprobarStock(pedidoModel);
+					DemandFullFill = true;
+				} else {
+					i++;
+				}
+				// implement exception that says "No more locals to ask for lotes"
+			}
 
-    @Override
-    public double getTotal(PedidoModel pedidoModel) {
+		}
 
-        return ((productoService.findById(pedidoModel.getIdProducto())).getPrecio() * pedidoModel.getCantidad());
-    }
+		return DemandFullFill;
+	}
+
+	@Override
+	public double getTotal(PedidoModel pedidoModel) {
+
+		return ((productoService.findById(pedidoModel.getIdProducto())).getPrecio() * pedidoModel.getCantidad());
+	}
 
 }
