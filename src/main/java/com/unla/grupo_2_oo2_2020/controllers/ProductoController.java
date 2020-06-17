e9bcfab04ce25b76ec72a44b895663178c9fb9ed
Nico <nicolas_lozano7@hotmail.com>
2020-06-16 21:30:30 -0300

    Excepciones para producto need fix

--- src/main/java/com/unla/grupo_2_oo2_2020/controllers/ProductoController.java
+++ src/main/java/com/unla/grupo_2_oo2_2020/controllers/ProductoController.java
@@ -7,12 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 import com.unla.grupo_2_oo2_2020.converters.ProductoConverter;
 import com.unla.grupo_2_oo2_2020.helpers.ViewRouteHelper;
 import com.unla.grupo_2_oo2_2020.models.ProductoModel;
@@ -76,22 +73,4 @@ import com.unla.grupo_2_oo2_2020.services.IProductoService;
 			return mAV;
 		}
 
-		@PostMapping("/create")
-		public RedirectView create(@ModelAttribute("producto") ProductoModel productoModel) {
-			productoService.insertOrUpdate(productoModel);
-			return new RedirectView(ViewRouteHelper.PRODUCTO_ROOT);
-		}
-
-		@PostMapping("/update")
-		public RedirectView update(@ModelAttribute("producto") ProductoModel productoModel) {
-			productoService.insertOrUpdate(productoModel);
-			return new RedirectView(ViewRouteHelper.PRODUCTO_ROOT);
-		}
-
-		@PostMapping("/remove/{idProducto}")
-		public RedirectView remove(@PathVariable("idProducto") long id) {
-			productoService.removeById(id);
-			return new RedirectView(ViewRouteHelper.PRODUCTO_ROOT);
-		}
-	}
-
+}
---
+++ src/main/resources/static/js/auth/Producto.js
@@ -0,0 +1,128 @@
+$(document).ready(function () {
+    $(".form-producto-remove").submit(function (event) {
+        event.preventDefault();
+
+        var dataArray = $(this).serializeArray(),
+        productoModel = {};
+
+        $(dataArray).each(function(i, field){
+        productoModel[field.name] = field.value;
+        });
+
+        removeProducto(prouctoModel);
+    });
+    $("#form-producto-create").submit(function (event) {
+        event.preventDefault();
+        var productoModel = {};
+
+        productoModel["nombre"] = $("#nombre").val();
+        productoModel["descripcion"] = $("#descripcion").val();
+        productoModel["precio"] = $("#precio").val();
+       
+
+        submitProducto(ProductoModel);
+    });
+    $("#form-producto-update").submit(function (event) {
+        event.preventDefault();
+        var productoModel = {};
+
+        productoModel["idProducto"] = $("#idProducto").val();
+        productoModel["nombre"] = $("#nombre").val();
+        productoModel["descripcion"] = $("#descripcion").val();
+        productoModel["precio"] = $("#precio").val();
+
+        submitProducto(productoModel);
+    });
+});
+
+function removeProducto(productoModel) {
+
+    $.ajax({
+        type: "DELETE",
+        contentType: "application/json",
+        data: JSON.stringify(empleadoModel),
+        url: "/api/producto/remove/" + productoModel.idProducto,
+        cache: false,
+        timeout: 600000,
+        success: function (data) {
+
+            if(data.success_removed) {
+
+                window.location.replace(data.redirect);
+            }
+        },
+    });
+}
+
+function submitProducto(productoModel) {
+
+    var url;
+
+    //si llega el id es porque estamos actualizando un cliente
+    if (productoModel.idProducto) {
+        url = "/api/producto/updateProducto";
+    } else {
+        url = "/api/producto/createProducto";
+    }
+
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: url,
+        data: JSON.stringify(productoModel),
+        cache: false,
+        timeout: 600000,
+        success: function (data) {
+            var json = data;
+            controlError(json);
+
+            if (json.success_created) {
+
+                $("#nombre").val("");
+                $("#descripcion").val("");
+                $("#precio").val("");
+
+                $("#feedback > div").html(json.success_created);
+            } else if (json.success_updated) {
+
+                $("#feedback > div").html(json.success_updated);
+            }
+
+            $("#feedback > div").addClass("alert alert-success");
+        },
+        error: function (e) {
+            var json = JSON.parse(e.responseText);
+            controlError(json);
+        },
+    });
+
+}
+
+function controlError(errors) {
+
+    $("#idProducto").removeClass("is-invalid");
+    $("#nombre").removeClass("is-invalid");
+    $("#decripcion").removeClass("is-invalid");
+    $("#precio").removeClass("is-invalid");
+
+    $("#feedback > div").html("");
+    $("#feedback > div").removeClass("alert alert-danger");
+    $("#feedback > div").removeClass("alert alert-success");
+
+
+    if (errors.product_already_exists) {
+        $("#nombre").addClass("is-invalid");
+        $("#feedback > div").addClass("alert alert-danger");
+        $("#feedback > div").html(errors.product_already_exists);
+    }
+   
+    if (errors.price_required) {
+        $("#precio").addClass("is-invalid");
+    }
+    if (errors.name_required) {
+        $("#nombre").addClass("is-invalid");
+    }
+    if (errors.description_required) {
+        $("#descripcion").addClass("is-invalid");
+    }
+}
